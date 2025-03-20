fcb32324786bc65e647717ccc71f5d28d354d708
Lance <enaganlance@gmail.com>
2025-03-21 00:10:31 +0800

    pseudocode

---
+++ batch_5_program_practice/6_digit_number.py
@@ -0,0 +1,9 @@
+# Prog02: Create a program that ask the user to input a number (0-1000). Print the number in 6 digit format. Add zeros at the beginning to complete the 6 digit.
+
+# ask the user to input a number between 1-1000
+# add zeros to the inputted number
+    # if the user entered a single digit it will add five zeros
+    # if the user entered two digits it will add four zeros
+    # if the user entered three digits it will add three zeros
+    # if the user entered four digits it will add two zeros
+# print the output

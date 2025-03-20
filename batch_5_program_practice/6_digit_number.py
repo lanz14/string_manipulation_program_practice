37a001bf3f49098151790010eca0250929f649bb
Lance <enaganlance@gmail.com>
2025-03-21 00:20:58 +0800

    adding zeros then print output

--- batch_5_program_practice/6_digit_number.py
+++ batch_5_program_practice/6_digit_number.py
@@ -5,6 +5,20 @@ number = int(input("Input a number (0-1000): "))
 
 # add zeros to the inputted number
     # if the user entered a single digit it will add five zeros, and print
+if 0 <= number <= 9:
+    print(f"00000{number}")
+
     # if the user entered two digits it will add four zeros, and print
+elif 10 <= number <= 99:
+    print(f"0000{number}")
+
     # if the user entered three digits it will add three zeros, and print
+elif 100 <= number <= 999:
+    print(f"000{number}")
+
     # if the user entered four digits it will add two zeros, and print
+elif number == 1000:
+    print(f"00{number}")
+
+else:
+    print(f"Numbers must be 1-1000 only.")

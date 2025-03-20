a760bb9d3d0ea989a8302971c203b3e0c612f932
Lance <enaganlance@gmail.com>
2025-03-21 00:38:29 +0800

    user's input

--- batch_5_program_practice/all_capital.py
+++ batch_5_program_practice/all_capital.py
@@ -1,5 +1,8 @@
 # Prog03: Create a program that ask the user to input their fullname. Print the input in all capital letter.
 
 # get the user's full name
+first_name = input("First name: ")
+last_name = input("Last name: ")
+
 # convert the input into a capitalized string
 # print the output all capitalized

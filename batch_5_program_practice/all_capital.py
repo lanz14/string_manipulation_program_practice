acd0c368293981f3ebc7993f2fba8d8942a90f3c
Lance <enaganlance@gmail.com>
2025-03-21 00:30:38 +0800

    pseudocode

---
+++ batch_5_program_practice/all_capital.py
@@ -0,0 +1,5 @@
+# Prog03: Create a program that ask the user to input their fullname. Print the input in all capital letter.
+
+# get the user's full name
+# convert the input into a capitalized string
+# print the output all capitalized

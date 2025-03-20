07feb2f975a39b2cda5e36ff358096d4b4d18712
Lance <enaganlance@gmail.com>
2025-03-21 01:12:50 +0800

    user's input

--- batch_5_program_practice/all_lowercase.py
+++ batch_5_program_practice/all_lowercase.py
@@ -1,5 +1,8 @@
 # Prog04: Create a program that ask the user to input their fullname. Print the input in all lower case.
 
 # user's input
+first_name = input("First name: ")
+last_name = input("Last name: ")
+
 # convert in input full name into lower case
 # print the output

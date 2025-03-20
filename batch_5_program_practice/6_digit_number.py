3407ad46543b5881ebc1644d1eb9e4f07b635d35
Lance <enaganlance@gmail.com>
2025-03-21 00:13:23 +0800

    pseudocode changes, and added user's input

--- batch_5_program_practice/6_digit_number.py
+++ batch_5_program_practice/6_digit_number.py
@@ -1,9 +1,10 @@
 # Prog02: Create a program that ask the user to input a number (0-1000). Print the number in 6 digit format. Add zeros at the beginning to complete the 6 digit.
 
 # ask the user to input a number between 1-1000
+number = int(input("Input a number (0-1000): "))
+
 # add zeros to the inputted number
-    # if the user entered a single digit it will add five zeros
-    # if the user entered two digits it will add four zeros
-    # if the user entered three digits it will add three zeros
-    # if the user entered four digits it will add two zeros
-# print the output
+    # if the user entered a single digit it will add five zeros, and print
+    # if the user entered two digits it will add four zeros, and print
+    # if the user entered three digits it will add three zeros, and print
+    # if the user entered four digits it will add two zeros, and print

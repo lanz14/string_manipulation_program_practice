c7dda6e148f19fe320d2b329ac532c9fdeef945d
Lance <enaganlance@gmail.com>
2025-03-20 17:50:36 +0800

    pseudocode

---
+++ batch_5_program_practice/fullname_no_spaces.py
@@ -0,0 +1,7 @@
+# Prog01: Create a program that ask the user to input their fullname with several space characters at the beginning. Print the input without the spaces in the beginning.
+
+# user's input
+    # first name
+    # last name
+# remove unnecessary spaces
+# print the full name with no spaces

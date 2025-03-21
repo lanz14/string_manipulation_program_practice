5786a19722451adeca8cb86a5807ddcf13aafd16
Lance <enaganlance@gmail.com>
2025-03-21 17:00:34 +0800

    pseudocode

---
+++ batch_5_program_practice/pascal_casing.py
@@ -0,0 +1,5 @@
+# Prog09: Create a program that ask the user to input their fullname in incorrect casing. Print the input in pascal case.
+
+# user's full name in incorrect casing
+# converting into pascal casing
+# print output

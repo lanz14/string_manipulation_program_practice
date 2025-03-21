bed0cd2b46bd50b2c91cd36c693d849d59cba946
Lance <enaganlance@gmail.com>
2025-03-21 16:21:02 +0800

    user's input

--- batch_5_program_practice/number_of_words.py
+++ batch_5_program_practice/number_of_words.py
@@ -1,5 +1,7 @@
 # Prog07: Create a program that ask the user to input a complete statement. Print the number of words in the input.
 
 # user's input
+sentence = input("Input a sentence: ")
+
 # count the words inputted
 # print the word count

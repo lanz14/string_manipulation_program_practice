9c3ce906855f0951282b8213400bde15a7a6734e
Lance <enaganlance@gmail.com>
2025-03-21 17:40:35 +0800

    pseudocode

---
+++ batch_5_program_practice/snake_case.py
@@ -0,0 +1,5 @@
+# Prog10: Create a program that ask the user to input their fullname in incorrect casing. Print the input in snake case.
+
+# user's full name
+# convert to snake case
+# print output

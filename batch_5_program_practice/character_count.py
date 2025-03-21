3a726f358dbe658ee730d73e47f0f31b852688a1
Lance <enaganlance@gmail.com>
2025-03-21 16:34:12 +0800

    user's input

--- batch_5_program_practice/character_count.py
+++ batch_5_program_practice/character_count.py
@@ -1,5 +1,8 @@
 # Prog08: Create a program that ask the user to input their fullname. Print the number of characters in the input.
 
 # user's full name
+first_name = input("First name: ")
+last_name = input("Last name: ")
+
 # count the characters
 # print the output

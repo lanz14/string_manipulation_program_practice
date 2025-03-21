3a582e5ed41d369a42d14f13b1592b70c5fa1928
Lance <enaganlance@gmail.com>
2025-03-21 15:57:12 +0800

    user's input

--- batch_5_program_practice/proper_casing.py
+++ batch_5_program_practice/proper_casing.py
@@ -1,5 +1,8 @@
 # Prog05: Create a program that ask the user to input their fullname in incorrect casing. Print the input in proper casing.
 
 # user's full name
+first_name = input("First name: ")
+last_name = input("Last name: ")
+
 # proper casing the inputted full name
 # print the proper cased full name

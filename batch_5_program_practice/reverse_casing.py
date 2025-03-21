f2a3b6c988aaef799636ab62f0ece47f7b1c0472
Lance <enaganlance@gmail.com>
2025-03-21 16:01:36 +0800

    pseudocode

---
+++ batch_5_program_practice/reverse_casing.py
@@ -0,0 +1,8 @@
+# Prog06: Create a program that ask the user to input their fullname in incorrect casing. Print each character of the input in reverse casing.
+
+# user's input of full name
+first_name = input("First name: ")
+last_name = input("Last name: ")
+
+# reverse the casing
+# print the output

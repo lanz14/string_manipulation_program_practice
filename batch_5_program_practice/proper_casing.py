6843472cfbcf9bfb7311481ae8687b4a0916e225
Lance <enaganlance@gmail.com>
2025-03-21 15:55:56 +0800

    pseudocode

---
+++ batch_5_program_practice/proper_casing.py
@@ -0,0 +1,5 @@
+# Prog05: Create a program that ask the user to input their fullname in incorrect casing. Print the input in proper casing.
+
+# user's full name
+# proper casing the inputted full name
+# print the proper cased full name

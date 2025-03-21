d8bb866d3c9639634ae48721cdfadd739ff9dc74
Lance <enaganlance@gmail.com>
2025-03-21 16:19:45 +0800

    pseudocode

---
+++ batch_5_program_practice/number_of_words.py
@@ -0,0 +1,5 @@
+# Prog07: Create a program that ask the user to input a complete statement. Print the number of words in the input.
+
+# user's input
+# count the words inputted
+# print the word count
